680185b21052a6b1e61efd541919fa3429bfb700
Rodrigo Rocha Sierra <rrs230780@gmail.com>
2025-03-01 11:40:34 -0500

    guardar3

--- logica_de_programacion/02_exercise.js
+++ logica_de_programacion/02_exercise.js
@@ -3,7 +3,7 @@ const nombre = prompt("¿Cuál es tu nombre?") || "desconocido";
 const edad = prompt("¿Cuántos años tienes?") || "desconocida";
 const lenguaje = prompt("¿Qué lenguaje de programación estudias?") || "un lenguaje desconocido";
 
-const mensaje = `Hola ${nombre}, tienes ${edad} años y ya estás aprendiendo ${lenguaje}!`;
+const mensaje = `Hola ${nombre}, tienes ${edad} años estás estudiando ${lenguaje}!`;
 
 alert(mensaje);
 
