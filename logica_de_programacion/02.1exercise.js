ec42a85344eabfd447cd8a3b5a29d212e80a707f
Rodrigo Rocha Sierra <rrs230780@gmail.com>
2025-03-01 12:17:08 -0500

    guardar5

--- logica_de_programacion/02.1exercise.js
+++ logica_de_programacion/02.1exercise.js
@@ -1,14 +1,17 @@
-const nombre = prompt("¿Cuál es tu nombre?");
-const edad = prompt("¿Cuántos años tienes?");
-const lenguaje = prompt("¿Qué lenguaje de programación estás estudiando?");
-const mensaje = "¡Hola ${nombre}, tienes ${edad} años y ya estás aprendiendo ${lenguaje}!";
+document.getElementById("iniciarPreguntas").addEventListener("click", function() {
+    const nombre = prompt("¿Cuál es tu nombre?") || "desconocido";
+    const edad = prompt("¿Cuántos años tienes?") || "desconocida";
+    const lenguaje = prompt("¿Qué lenguaje de programación estudias?") || "un lenguaje desconocido";
+    
+    const mensaje = `Hola ${nombre}, tienes ${edad} años y estás estudiando ${lenguaje}!`;
+    alert(mensaje);
 
-alert(mensaje);
-
-const gusta = prompt("¿Te gusta estudiar ${lenguaje}? Responde con el número 1 - SÍ o 2 - NO");
-if (gusta == 1){
-    alert("¡Muy bien! Sigue estudiando y tendrás mucho éxito.");
-}
-if (gusta == 2){
-    alert("Oh, qué pena... ¿Has intentado aprender otros lenguajes?");
-}
+    const gusta = prompt(`¿Te gusta estudiar ${lenguaje}? Responde con el número 1 - SÍ o 2 - NO`);
+    if (gusta == 1) {
+        alert("¡Muy bien! Sigue estudiando y tendrás mucho éxito.");
+    } else if (gusta == 2) {
+        alert("Lo siento... ¿Has intentado aprender otros lenguajes?");
+    } else {
+        alert("Respuesta no válida. Por favor, intenta de nuevo.");
+    }
+});
