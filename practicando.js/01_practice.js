f2c2da571925e0833176ff719790c4aa4ee7f4d1
Rodrigo <rrs230780@gmail.com>
2025-03-13 09:38:14 -0500

    guardar_siiii

---
+++ practicando.js/01_practice.js
@@ -0,0 +1,15 @@
+let a = 23;
+let b = 45;
+
+b = 54;
+
+suma = (a <= b);
+
+
+console.log(suma);
+
+nombre = 'rodrigo';
+apellido = 'rocha';
+
+nombre_completo = `${nombre} ${apellido}`;
+console.log(nombre_completo);
